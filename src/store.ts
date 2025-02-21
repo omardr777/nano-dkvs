6e72deb31f00fac3e6edb52e98b07e5981a6d557
Ali <ali0fawzish@outlook.com>
2025-02-21 18:21:07 +0300

    feat: allow custom port configuration for Node and add server registration method in Store class

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,11 @@
 export class Store {
   private store = new Map<string, string | number>();
+  private servers = new Map<string, number>();
+
+  register(server: string, port: number) {
+    return this.servers.set(server, port);
+  }
+
   set(key: string, value: string | number) {
     this.store.set(key, value);
     return `${key} set to ${value}`;
